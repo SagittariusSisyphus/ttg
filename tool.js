fd18413558a0bee211600a53f8f37a6612e9bd06
gyxOnly <430960359@qq.com>
2023-09-26 20:30:47 +0800

    tool 文件编辑完成

--- tool.js
+++ tool.js
@@ -5,4 +5,18 @@ const pool = mysql.createPool({
     password: "123abc",
     database: "student",
 })
-exports.module = mysql;
+exports.module = (sql, arr = []) => {
+    return new Promise((resolve, reject) => {
+        pool.getConnection((err, connection) => {
+            if (err) {
+                reject(err)
+            }
+            connection.release();
+            connection.query(sql, arr, (e, res) => {
+                if (e) reject(e)
+                resolve(res)
+            })
+        });
+
+    })
+}
